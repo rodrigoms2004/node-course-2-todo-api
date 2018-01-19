016357c913461c10c03b0c257e9be6cbc08ca38a
Rodrigo <rodrigoms2004@gmail.com>
2018-01-19 15:56:41 -0200

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -52,6 +52,25 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.methods.removeToken = function (token) {
+  var user = this;
+
+  return user.update({
+    $pull: {
+     tokens: {token}
+    }
+  });
+};
+
+/*
+tokens: {
+  token: token
+}
+...OR...
+tokens: {token}
+
+
+*/
 UserSchema.statics.findByToken = function (token) {
   var User = this;
   var decoded;
